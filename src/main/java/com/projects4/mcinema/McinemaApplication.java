407a457b1fe16e596bd48f6d898d8d038ff80add
longtran <huylong.vipandrip@gmail.com>
2020-10-22 11:01:49 +0700

    Initial commit

---
+++ src/main/java/com/projects4/mcinema/McinemaApplication.java
@@ -0,0 +1,13 @@
+package com.projects4.mcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects4/mcinema/McinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects4.mcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
